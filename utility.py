4c1a79a6b9bb76714cd875f9a604fbd9ee667de2
gpoon1 <131670987+gpoon1@users.noreply.github.com>
2023-08-12 20:53:17 +0100

    Add files via upload

---
+++ utility.py
@@ -0,0 +1,34 @@
+
+import yaml
+import logging
+
+def read_config_file(filepath):
+    with open(filepath, 'r') as stream:
+        try:
+            return yaml.load(stream, Loader=yaml.Loader)
+        except yaml.YAMLError as exc:
+            logging.error(exc)
+
+def col_header_val(df, table_config):
+    df.columns = df.columns.str.lower()
+    df.columns = df.columns.str.replace('[^\w]', '_', regex=True)
+    df.columns = df.columns.str.strip('_')
+    
+    expected_col = list(map(str.lower, table_config['columns']))
+    expected_col.sort()
+    
+    df.columns = list(map(str.lower, df.columns))
+    df = df.reindex(sorted(df.columns), axis=1)
+    
+    if len(df.columns) == len(expected_col) and list(expected_col) == list(df.columns):
+        print("Column name and column length validation passed")
+        return 1
+    else:
+        print("Column name and column length validation failed")
+        mismatched_columns_file = list(set(df.columns).difference(expected_col))
+        print("Following File columns are not in the YAML file:", mismatched_columns_file)
+        missing_YAML_file = list(set(expected_col).difference(df.columns))
+        print("Following YAML columns are not in the file uploaded:", missing_YAML_file)
+        logging.info(f'df columns: {df.columns}')
+        logging.info(f'expected columns: {expected_col}')
+        return 0
